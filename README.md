e1cdeab6e7e309f23e04d2a639c56b8b4ab24d47
Korhonen Onerva <aokorhon@cs-013.org.aalto.fi>
2018-05-04 15:56:15 +0300

    wrote basic functions for reading nodes & links from csv and for creating the network

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # climate-companions-bipartite
 Code for investigating the even co-participation based bipartite graph structure in the Ilmastokumppanit (Climate Companions) city-to-business network. Should be applicable also to other bipartite module analyses.
+
+functions.py contains functions needed for creating and analysing the bipartite network. The functions can be called from either a frontend script (an example will be added in future) or interactively.
---
+++ functions.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  4 12:24:58 2018
+
+@author: aokorhon
+
+This file contains functions related to the Ilmastokumppanit bipartite project. To call, either write a 
+frontend script (see XX for example) or call interactively from ipython.
+"""
+import pandas as pd
+import networkx as nx
+from networkx.algorithms import bipartite
+
+
+# Functions for reading metadata
+
+def readNodes(cfg):
+    """
+    Reads information about network nodes and their attributes. To this end, the information
+    must be stored in a csv file with named columns. By default, column names are set for reading
+    company information ('Alias' (anonymized alias used to refer to the
+    node in the network), 'Name' (real name of the company), and 'FoB' (field of business)).
+    Other sets of column names can be given as parameters. Data of the first column will be used
+    as node labels and must therefore be anonymized.
+    
+    Parameters:
+    -----------
+    cfg: dic, contains:
+        inputPath: str, path to the input csv file
+        columnNames: list of strs, column names in the csv file (default ['Alias', 'Name', 'FoB'])
+        
+    Returns:
+    --------
+    nodeInfo: 2D list, first element contains node aliases and further elements additional attributes
+    columnNames: list of strs, column names of the csv file (and therefore titles of the lists in nodeInfo)
+    
+    """
+    inputPath = cfg['inputPath']
+    if 'columnNames' in cfg.keys():
+        columnNames= cfg['columnNames']
+    else:
+        columnNames = ['Alias', 'Name', 'FoB']
+    
+    nodeData = pd.read_csv(inputPath)
+    nodeInfo = []
+    
+    #import pdb; pdb.set_trace()
+    
+    for columnIndex, columnName in enumerate(columnNames):
+        dlist = []
+        data = nodeData[columnName]
+        for i in data:
+            dlist.append(i)
+        nodeInfo.append(dlist)
+        
+    return nodeInfo, columnNames
+    
+def readLinks(cfg):
+    """
+    Reads the link information from a csv file. The default column names are ['Source', 'Target'] but other
+    names can be given as parameters. Note that the identifiers of source and target nodes in the file must
+    equal to nodes' aliases.
+    
+    Parameters:
+    -----------
+    cfg: dic, contains:
+        inputPath: str, path to the input csv file
+        
+    Returns:
+    --------
+    links: list of (source,target) tuples
+    """
+    inputPath = cfg['inputPath']
+    if 'columnNames' in cfg.keys():
+        columnNames= cfg['columnNames']
+    else:
+        columnNames = ['Source', 'Target']
+    
+    linkData = pd.read_csv(inputPath)
+    links = []
+    
+    sources = linkData[columnNames[0]]
+    targets = linkData[columnNames[1]]
+    
+    for source, target in zip(sources,targets):
+        links.append((source,target))
+    
+    return links
+
+    
+    
+    
+    
+    
+# Functions for network construction
+    
+def addNodes(cfg, net, bipartite):
+    """
+    Based on the given metadata, adds a set of nodes to a bipartite graph.
+    
+    Parameters:
+    -----------
+    cfg: dic, contains:
+        inputPath: str, path to the metadata input csv file
+        columnNames: list of strs, column names in the csv file
+        net: nx.Graph()
+        bipartite: int (0 or 1), bipartite node class
+        
+    Returns:
+    --------
+        net: nx.Graph()
+    """
+    # Reading node information
+    nodeInfo, columnNames = readNodes(cfg)
+    aliases = nodeInfo[0] # first column is used as node alias
+    attributes = nodeInfo[1::] # additional columns contain attributes
+    
+    # Adding nodes and updating their attributes
+    for i, node in enumerate(aliases):
+        net.add_node(node, bipartite=bipartite)
+        attr_dic = {}
+        for columnName, attribute in zip(columnNames[1::], attributes):
+            attr_dic[columnName] = attribute[i]
+        net.nodes[node].update(attr_dic)
+        
+    return net
+    
+def createMultilayer(cfg):
+    """
+    Creates a multilayer network with two types of nodes (companies and events), node attributes (real names, fields of business, ...), 
+    and links between different types of nodes.
+    
+    Parameters:
+    -----------
+    cfg: dic, contains:
+        companyInputPath: str, path to the company metadata input csv file
+        eventInputPath: str, path to the event metadata input csv file
+        linkInputPath: str, path to the link input csv file
+        companyColumnNames: list of strs, column names in the csv file containing the company information (default ['Alias', 'Name', 'FoB'])
+        eventColumnNames: list of strs, column names in the csv file containing the company information (default ['Alias', 'Name'])
+        linkColumnNames: list of strs, column names in the link csv file (default ['Source, 'Target'])
+        
+    Returns:
+    --------
+    mnet: networkX.Graph(), a bipartite
+    """
+    mnet = nx.Graph()
+    
+    # Reading company information, adding company nodes
+    cfg['inputPath'] = cfg['companyInputPath']
+    if 'companyColumnNames' in cfg.keys():
+        cfg['columnNames'] = cfg['companyColumnNames']
+    else:
+        cfg['columnNames'] = ['Alias', 'Name', 'FoB']
+    mnet = addNodes(cfg, mnet, bipartite=0)    
+    
+    # Reading event information, adding event nodes
+    cfg['inputPath'] = cfg['eventInputPath']
+    if 'eventColumnNames' in cfg.keys():
+        cfg['columnNames'] = cfg['eventColumnNames']
+    else:
+        cfg['columnNames'] = ['Alias', 'Name']
+    mnet = addNodes(cfg, mnet, bipartite=1)
+    
+    # Reading link data, adding links
+    cfg['inputPath'] = cfg['linkInputPath']
+    if 'linkColumnNames' in cfg.keys():
+        cfg['columnNames'] = cfg['linkColumnNames']
+    else:
+        cfg['columnNames'] = ['Source','Target']
+    links = readLinks(cfg)
+    mnet.add_edges_from(links)
+    
+    
+    return mnet
+    
+    
+    
+
+
+
+
+    
+        
+
+    
+    
